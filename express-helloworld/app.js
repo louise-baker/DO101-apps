b4e6cafa24ca0f943f1e1c088c539c53e5afdefc
louise-baker <louise.baker01@gmail.com>
2020-06-19 19:06:38 +1000

    For fun

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,14 @@ app.get('/venus', function (req, res) {
   res.send('Hello Venus!\n');
 });
 
+app.get('/jake', function (req, res) {
+  res.send('I wuv you!!!!\n');
+});
+
+app.get('/rob', function (req, res) {
+  res.send('Booo!!!!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
